b21269e6be3f4cd518b463edc166b8296c752c12
dn0912 <nguyen.duc0912@googlemail.com>
2017-04-09 15:06:24 +0200

    test.js

---
+++ test.js
@@ -0,0 +1,19 @@
+var expect = chai.expect;
+
+describe('MyAPI', function() {
+  var xhr;
+  var requests;
+  beforeEach(function() {
+    xhr = sinon.useFakeXMLHttpRequest();
+
+    requests = [];
+    xhr.onCreate = function(xhr) {
+      requests.push(xhr);
+    };
+  });
+
+  afterEach(function() {
+    xhr.restore();
+  });
+  
+});
